bfa583ba774da39b25edf2a1ef9d17d6b1f231a6
Marcel Cruz <marcelcruz@live.com>
2018-02-23 12:31:30 +0000

    component files added

--- src/app.js
+++ src/app.js
@@ -1,49 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from dashboard
-  </div>
-);
-
-const AddExpensePage = () => (
-  <div>
-    This is AddExpensePage
-  </div>
-);
-
-const EditExpensePage = () => (
-  <div>
-    This is edit EditExpensePage
-  </div>
-);
-
-const HelpPage = () => (
-  <div>
-    This is HelpPage
-  </div>
-);
-
-const NotFoundPage = () => (
-  <div>
-    404!
-  </div>
-);
-
-const routes = (
-  <BrowserRouter>
-    <Switch>
-      <Route path='/' component={ExpenseDashboardPage} exact={true} />
-      <Route path='/create' component={AddExpensePage} />
-      <Route path='/edit' component={EditExpensePage} />
-      <Route path='/help' component={HelpPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+  <div>
+    This is edit EditExpensePage
+  </div>
+);
+
+export default EditExpensePage;
