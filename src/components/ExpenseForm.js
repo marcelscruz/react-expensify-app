f9cb70853c68f55ece8e6d11cc5af85f8a39e78c
Marcel Cruz <marcelcruz@live.com>
2018-02-26 17:15:22 +0000

    EditExpensePage configured

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -15,14 +15,18 @@ ThemedStyleSheet.registerTheme(DefaultTheme);
 console.log(moment());
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
-    calendarFocused: false,
-    error: ''
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calendarFocused: false,
+      error: ''
+    };
+  }
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({ description }));
@@ -56,7 +60,7 @@ export default class ExpenseForm extends React.Component {
       this.setState(() => ({ error: '' }) );
       this.props.onSubmit({
         description: this.state.description,
-        amount: parseFloat(this.state.amount, 10) * 100, 
+        amount: parseFloat(this.state.amount, 10) * 100,
         createdAt: this.state.createdAt.valueOf(),
         note: this.state.note
       });
